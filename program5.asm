019b2560bc944842e91a894559f926a2c9b02a5b
Jeo Vela <37819992+jeovela@users.noreply.github.com>
2021-06-29 15:17:20 -0500

    Update program5.asm

--- program5.asm
+++ program5.asm
@@ -15,14 +15,14 @@ INCLUDE Irvine32.inc
 	;	This macro displays a string.
 	;	preconditions: the OFFSET of the string must be pushed prior to invoking MACRO
 	;	receives: 
-	;			[ebp+ ] = some prompt				;did not include # w/ ebp+ as I use a variety of different prompts that are pushed in different locations on stack
+	;		[ebp+ ] = some prompt				;did not include # w/ ebp+ as I use a variety of different prompts that are pushed in different locations on stack
 	;	returns: outputs a string
 	;********************
 	displayString	MACRO	memoryAddress			
 		push	edx
-		mov		edx, memoryAddress
+		mov	edx, memoryAddress
 		call	WriteString
-		pop		edx
+		pop	edx
 	ENDM
 
 	;*******************
@@ -30,20 +30,20 @@ INCLUDE Irvine32.inc
 	;		This macro prompts user to enter an unsigned int and reads the value entered as a string.
 	;		preconditions:  the address of a prompt, array and size must be pushed prior to invoking MACRO.
 	;		receives:
-	;				[ebp+16] = prompt2
-	;				[ebp+8] = OFFSET listArray
-	;				[ebp+24] = SIZEOF listArray
+	;			[ebp+16] = prompt2
+	;			[ebp+8] = OFFSET listArray
+	;			[ebp+24] = SIZEOF listArray
 	;		returns: generates a string
 	;********************
 	getString	MACRO	prompt, memoryAddress, size
 		displayString	prompt
 		push	ecx
 		push	edx
-		mov		edx, memoryAddress				;memoryAddress = destination string
-		mov		ecx, size
+		mov	edx, memoryAddress				;memoryAddress = destination string
+		mov	ecx, size
 		call	ReadString						;call to get num from user in string form & save to destination string, memoryAddress which is in edx register
-		pop		edx
-		pop		ecx
+		pop	edx
+		pop	ecx
 	ENDM
 
 ; (insert constant definitions here)
@@ -73,10 +73,10 @@ INCLUDE Irvine32.inc
 	prompt7		BYTE	"Thanks for playing!",0
 
 	inputArray	DWORD	10	DUP(?)
-	outputArray DWORD	10	DUP(?)
+	outputArray 	DWORD	10	DUP(?)
 	listArray	DWORD	10	DUP(?)
-	sum			DWORD	?
-	avg			DWORD	?
+	sum		DWORD	?
+	avg		DWORD	?
 
 .code
 main PROC
@@ -124,17 +124,17 @@ main ENDP
 ;*********************
 ;introduction- This procedure displays the title of the program, author, and the directions.
 ;		receives: 
-;				[ebp+20] = OFFSET intro
-;				[ebp+16] = OFFSET author
-;				[ebp+12] = OFFSET space
-;				[ebp+8]  = OFFSET prompt1
+;			[ebp+20] = OFFSET intro
+;			[ebp+16] = OFFSET author
+;			[ebp+12] = OFFSET space
+;			[ebp+8]  = OFFSET prompt1
 ;		returns: nothing
 ;		preconditions: offsets of strings need to be pushed on stack frame to use with MACRO displayString
 ;		postconditions: none
 ;*********************
 introduction PROC
 	push	ebp
-	mov		ebp, esp
+	mov	ebp, esp
 
 	displayString	[ebp+20]
 	call	Crlf
@@ -154,70 +154,70 @@ introduction ENDP
 ;*********************
 ;readVal - This procedure is called 10 times from getUserNum. It invokes the MACRO getString to get user number in string format and then validates the string in the procedure.
 ;		receives: 
-;				[ebp+8] = OFFSET listArray
-;				[ebp+12] = edi				;current element
-;				[ebp+16] = OFFSET prompt2
-;				[ebp+20] = OFFSET errorMsg
-;				[ebp+24] = SIZEOF listArray
+;			[ebp+8] = OFFSET listArray
+;			[ebp+12] = edi				;current element
+;			[ebp+16] = OFFSET prompt2
+;			[ebp+20] = OFFSET errorMsg
+;			[ebp+24] = SIZEOF listArray
 ;		returns: a valid number in string form to the proper position passed in the array
 ;		preconditions: proper array postion must be passed to getString from getUserNum (ie edi)
 ;		postconditions: registers changed: eax, ebx, ecx, edx, esi
 ;********************* 
 readVal PROC 
 	push	ebp
-	mov		ebp, esp
+	mov	ebp, esp
 
 	pushad 
 
 	getString	[ebp+16], [ebp+8], [ebp+24]				;prompt2, listArray, SIZEOF listArray
 
 	start:	
-		mov		esi, [ebp+8]			; point source index pointer to listArray
-		mov		eax, 0
-		mov		ebx, 1
-		mov		ecx, 0
-		mov		edx, 0
+		mov	esi, [ebp+8]			; point source index pointer to listArray
+		mov	eax, 0
+		mov	ebx, 1
+		mov	ecx, 0
+		mov	edx, 0
 		cld
 
 	chkForEndOfStr:
 		lodsb
-		cmp		ax, 0
-		je		checkStrLength				
-		inc		ecx
-		jmp		chkForEndOfStr				;setup to loop until end of str
+		cmp	ax, 0
+		je	checkStrLength				
+		inc	ecx
+		jmp	chkForEndOfStr				;setup to loop until end of str
 
 	checkStrLength:
-		cmp		ecx, 10						;compare if # will fit in 10 digits, 32-bit fits in 10 digits
-		jg		invalid
-		mov		esi, [ebp+8]				;point to front of string
-		add		esi, ecx
-		dec		esi	
+		cmp	ecx, 10						;compare if # will fit in 10 digits, 32-bit fits in 10 digits
+		jg	invalid
+		mov	esi, [ebp+8]				;point to front of string
+		add	esi, ecx
+		dec	esi	
 		std	
 
 	keepReadingStr:
 		lodsb
-		cmp		ax, 48						;compare to ASIIC code # 48, which equals 0
-		jl		invalid
-		cmp		ax, 57						;compare to ASIIC code # 57, which equals 9
-		jg		invalid
+		cmp	ax, 48						;compare to ASIIC code # 48, which equals 0
+		jl	invalid
+		cmp	ax, 57						;compare to ASIIC code # 57, which equals 9
+		jg	invalid
 
-		sub		ax, 48
+		sub	ax, 48
 		push	edx
-		mul		ebx
-		pop		edx	
-		jc		invalid						;jmp if carry (CF)
+		mul	ebx
+		pop	edx	
+		jc	invalid						;jmp if carry (CF)
 
-		add		edx, eax
-		jc		invalid
+		add	edx, eax
+		jc	invalid
 		push	edx
 	
-		mov		eax, ebx
-		mov		ebx, 10
-		mul		ebx
-		mov		ebx, eax
+		mov	eax, ebx
+		mov	ebx, 10
+		mul	ebx
+		mov	ebx, eax
 
-		mov		eax, 0
-		pop		edx
+		mov	eax, 0
+		pop	edx
 	loop	keepReadingStr
 	
 	jmp		valid
@@ -225,42 +225,42 @@ readVal PROC
 	invalid:
 		displayString	[ebp+20]
 		call	CrLf
-		getString		[ebp+16], [ebp+8], [ebp+24]
-		jmp		start
+		getString	[ebp+16], [ebp+8], [ebp+24]
+		jmp	start
 
 	valid:	
-		mov		ebx, [ebp+12]
-		mov		[ebx], edx
+		mov	ebx, [ebp+12]
+		mov	[ebx], edx
 
 	finished:
 		popad
-		pop		ebp
-		ret		20
+		pop	ebp
+		ret	20
 readVal ENDP
 
 ;*********************
 ;getUserNums - This procedure proceeds to gather the user's number by calling the readVal procedure 10 times to fill an array of size 10 and also calculate the sum.
 ;		receives: 
-;				[ebp+8] = OFFSET listArray
-;				[ebp+12] = OFFSET outputArray
-;				[ebp+16] = OFFSET inputArray
-;				[ebp+20] = OFFSET sum
-;				[ebp+24] = OFFSET errorMsg
-;				[ebp+28] = OFFSET prompt2 - asks user to enter unsigned int
-;				[ebp+32] = SIZEOF listArray
+;			[ebp+8] = OFFSET listArray
+;			[ebp+12] = OFFSET outputArray
+;			[ebp+16] = OFFSET inputArray
+;			[ebp+20] = OFFSET sum
+;			[ebp+24] = OFFSET errorMsg
+;			[ebp+28] = OFFSET prompt2 - asks user to enter unsigned int
+;			[ebp+32] = SIZEOF listArray
 ;		returns: an array filled with values and the sum
 ;		preconditions:	listArray must be uninitialized
 ;		postconditions: registers changed: eax, ebx, ecx, edi
 ;*********************
 getUserNums	PROC
 	push	ebp
-	mov		ebp, esp
+	mov	ebp, esp
 
 	pushad
 
-	mov		edi, [ebp+8]				;setting first element of array into edi register
-	mov		ebx, 0						;sum counter
-	mov		ecx, 10						;loop counter
+	mov	edi, [ebp+8]				;setting first element of array into edi register
+	mov	ebx, 0						;sum counter
+	mov	ecx, 10						;loop counter
 
 	start:
 		push	[ebp+32]
@@ -269,16 +269,16 @@ getUserNums	PROC
 		push	edi						;current element
 		push	[ebp+16]
 		call	readVal
-		add		ebx, [edi]				;add value in edi to ebx to keep track of the sum
-		add		edi, 4					;add 4 to edi to move to next memory location in array
+		add	ebx, [edi]				;add value in edi to ebx to keep track of the sum
+		add	edi, 4					;add 4 to edi to move to next memory location in array
 	loop	start
 
 	finished:
-		mov		eax, [ebp+20]			;sum address moved to eax
-		mov		[eax], ebx				;move value of sum counter, ebx, to the variable sum
+		mov	eax, [ebp+20]			;sum address moved to eax
+		mov	[eax], ebx			;move value of sum counter, ebx, to the variable sum
 		popad
-		pop		ebp
-		ret		28
+		pop	ebp
+		ret	28
 getUserNums ENDP
 
 ;******************
@@ -292,25 +292,25 @@ getUserNums ENDP
 ;**********************
 calculateAvg PROC
 	push	ebp
-	mov		ebp, esp
+	mov	ebp, esp
 
 	push	eax
 	push	ebx
 	push	edx
 
-	mov		eax, [ebp+8]
-	mov		ebx, 10				
-	mov		edx, 0		
-	div		ebx						;eax/ebx
+	mov	eax, [ebp+8]
+	mov	ebx, 10				
+	mov	edx, 0		
+	div	ebx						;eax/ebx
 
-	mov		ebx, [ebp+12]
-	mov		[ebx], eax				;avg variable now has value
+	mov	ebx, [ebp+12]
+	mov	[ebx], eax				;avg variable now has value
 
-	pop		edx
-	pop		ebx
-	pop		eax
-	pop		ebp
-	ret		8
+	pop	edx
+	pop	ebx
+	pop	eax
+	pop	ebp
+	ret	8
 calculateAvg ENDP
 
 ;**********************
@@ -331,7 +331,7 @@ calculateAvg ENDP
 ;*********************
 printValues PROC
 	push	ebp
-	mov		ebp, esp
+	mov	ebp, esp
 
 	call	Crlf
 	call	Crlf
@@ -377,13 +377,13 @@ printValues ENDP
 ;***********************
 printArray PROC
 	push	ebp
-	mov		ebp, esp
+	mov	ebp, esp
 
 	push	esi
 	push	ecx
 
-	mov		esi, [ebp+12]				;source pointer pointing to listArray
-	mov		ecx, 10						;loop counter
+	mov	esi, [ebp+12]				;source pointer pointing to listArray
+	mov	ecx, 10						;loop counter
 
 	print:
 		push	[esi]					;pass current element
@@ -400,10 +400,10 @@ printArray PROC
 										;left vacant to signify the intentional skip
 			loop	print
 
-	pop		ecx
-	pop		esi
-	pop		ebp
-	ret		16
+	pop	ecx
+	pop	esi
+	pop	ebp
+	ret	16
 printArray ENDP
 
 ;****************************
@@ -418,37 +418,36 @@ printArray ENDP
 ;****************************
 WriteVal PROC
 
-	push			ebp
-	mov				ebp, esp
+	push	ebp
+	mov	ebp, esp
 
 	pushad
 
-	mov		eax, [ebp+16]			;number to print moved to eax
-	mov		edi, [ebp+12]
-	mov		ecx, 0					; counter
+	mov	eax, [ebp+16]			;number to print moved to eax
+	mov	edi, [ebp+12]
+	mov	ecx, 0					; counter
 	cld
 	
 	convert:
-		mov		edx, 0
-		mov		ebx, 10
-		div		ebx
-		mov		ebx, edx
-		add		ebx, 48
+		mov	edx, 0
+		mov	ebx, 10
+		div	ebx
+		mov	ebx, edx
+		add	ebx, 48
 		push	eax
-		mov		eax, ebx
+		mov	eax, ebx
 		stosb
-		pop		eax	
-		inc		ecx
-		cmp		eax, 0
-		je		finished
-		jmp		convert						;jump if more #'s to convert
+		pop	eax	
+		inc	ecx
+		cmp	eax, 0
+		je	finished
+		jmp	convert						;jump if more #'s to convert
 
-	finished:								;string now reversed
-		;stosb								;move 0 to edi to indicate end of string
-		mov		esi, [ebp+12]
-		add		esi, ecx
-		dec		esi
-		mov		edi, [ebp+8]
+	finished:							;string now reversed
+		mov	esi, [ebp+12]
+		add	esi, ecx
+		dec	esi
+		mov	edi, [ebp+8]
 
 	flipStr:								;Used from demo program6 to reverse digits in string
 		std
@@ -457,13 +456,13 @@ WriteVal PROC
 		stosb
 	loop	flipStr
 
-	mov		eax, 0							;move a 0 to denote end of string
+	mov	eax, 0							;move a 0 to denote end of string
 	stosb									;move to edi
 	displayString	[ebp+8]
 	
 	popad								
-	pop		ebp
-	ret		12					
+	pop	ebp
+	ret	12					
 WriteVal ENDP
 
 ;***************
@@ -476,14 +475,14 @@ WriteVal ENDP
 ;*****************
 farewell PROC
 	push	ebp
-	mov		ebp, esp
+	mov	ebp, esp
 
 	call	Crlf
 	displayString	[ebp+8]
 	call	Crlf
 
-	pop		ebp
-	ret		4
+	pop	ebp
+	ret	4
 farewell ENDP
 
 END main
