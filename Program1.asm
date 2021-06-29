8defb3ff79edcd274ac285ed99d2d93ad47fd31b
Jeo Vela <37819992+jeovela@users.noreply.github.com>
2021-06-29 14:58:04 -0500

    Update Program1.asm

--- Program1.asm
+++ Program1.asm
@@ -45,131 +45,131 @@ main PROC
 
 ; (insert executable instructions here)
 ;displays header to user
-mov		edx, OFFSET heading			;moving the string called heading to register edx to be prepared for WriteString directive
+mov	edx, OFFSET heading			;moving the string called heading to register edx to be prepared for WriteString directive
 call	WriteString
 call	Crlf
 
 ;display a line of space between header and instructions
-mov		edx, OFFSET sentSpace
+mov	edx, OFFSET sentSpace
 call	WriteString
 call	Crlf
 
 ;display instructions to user to enter 2 numbers
-mov		edx, OFFSET instructions	;sending memory location of instruction to edx register to be called upon with WriteString
+mov	edx, OFFSET instructions	;sending memory location of instruction to edx register to be called upon with WriteString
 call	WriteString
 call	Crlf
 
 ;display space
-mov		edx, OFFSET sentSpace
+mov	edx, OFFSET sentSpace
 call	WriteString
 call	Crlf
 
 ;read first integer entered by user into memory and display the number
-mov		edx, OFFSET prompt1
+mov	edx, OFFSET prompt1
 call	WriteString
 call	ReadInt					;read first integer input by user
-mov		firstInt, eax			;move from eax register to firstInt variable
+mov	firstInt, eax			;move from eax register to firstInt variable
 
 ;read second integer entered by user into memory and display the number
-mov		edx, OFFSET prompt2
+mov	edx, OFFSET prompt2
 call	WriteString
 call	ReadInt				;read first integer input by user
-mov		secInt, eax			;move from eax register to secInt variable
+mov	secInt, eax			;move from eax register to secInt variable
 
 ;display space
-mov		edx, OFFSET sentSpace
+mov	edx, OFFSET sentSpace
 call	WriteString
 call	Crlf
 
 ;add the two numbers
-mov		eax, firstInt	;move firstInt to eax register
-add		eax, secInt		;add secInt to firstInt located in eax
-mov		addResult, eax	;move the result from adding the two numbers from the eax register to the variable, addResult
+mov	eax, firstInt	;move firstInt to eax register
+add	eax, secInt		;add secInt to firstInt located in eax
+mov	addResult, eax	;move the result from adding the two numbers from the eax register to the variable, addResult
 
 ;subtract the two numbers
-mov		eax, firstInt
+mov	eax, firstInt
 sub     eax, secInt
-mov		subResult, eax		;move the result from subtracting the two numbers from the eax register to the variable, subResult
+mov	subResult, eax		;move the result from subtracting the two numbers from the eax register to the variable, subResult
 
 ;mulitply the two numbers
-mov		eax, firstInt
-mul		secInt					;multiplying the secInt with the firstInt located in register eax
-mov		multiplyResult, eax		;move the result from subtracting the two numbers from the eax register to the variable, multiplyResult
+mov	eax, firstInt
+mul	secInt					;multiplying the secInt with the firstInt located in register eax
+mov	multiplyResult, eax		;move the result from subtracting the two numbers from the eax register to the variable, multiplyResult
 
 ;divide both numbers
-mov		eax, firstInt
-div		secInt					;dividing the secInt with the firstInt located in register eax
-mov		divisionResult, eax
-mov		remainder, edx			;edx register holds the remainder from division, so moving that value to the variable, remainder
+mov	eax, firstInt
+div	secInt					;dividing the secInt with the firstInt located in register eax
+mov	divisionResult, eax
+mov	remainder, edx			;edx register holds the remainder from division, so moving that value to the variable, remainder
 
 ;display the result of adding the two integers in equation form
-mov		eax, firstInt				;display first integer
+mov	eax, firstInt				;display first integer
 call	WriteDec
-mov		edx, OFFSET addSign			;display add sign
+mov	edx, OFFSET addSign			;display add sign
 call	WriteString
-mov		eax, secInt					;display second integer
+mov	eax, secInt					;display second integer
 call	WriteDec
-mov		edx, OFFSET equalSign		;display equal sign
+mov	edx, OFFSET equalSign		;display equal sign
 call	WriteString
-mov		eax, addResult				;display sum
+mov	eax, addResult				;display sum
 call	WriteDec					
 call	Crlf
 
 ;display the result of subtracting the two integers in equation form
-mov		eax, firstInt			;display first integer
+mov	eax, firstInt			;display first integer
 call	WriteDec
-mov		edx, OFFSET minusSign	;display subtraction sign
+mov	edx, OFFSET minusSign	;display subtraction sign
 call	WriteString
-mov		eax, secInt				;display second integer
+mov	eax, secInt				;display second integer
 call	WriteDec
-mov		edx, OFFSET equalSign	;display equal sign
+mov	edx, OFFSET equalSign	;display equal sign
 call	WriteString
-mov		eax, subResult			;display result of subtraction
+mov	eax, subResult			;display result of subtraction
 call	WriteDec
 call	Crlf
 
 ;display the result of multiplying the two numbers
-mov		eax, firstInt				;display first int
+mov	eax, firstInt				;display first int
 call	WriteDec
-mov		edx, OFFSET multiplySign	;display multiply sign
+mov	edx, OFFSET multiplySign	;display multiply sign
 call	WriteString
-mov		eax, secInt					;display second int
+mov	eax, secInt					;display second int
 call	WriteDec
-mov		edx, OFFSET equalSign		;display equal sign
+mov	edx, OFFSET equalSign		;display equal sign
 call	WriteString
-mov		eax, multiplyResult			;display multiplication result
+mov	eax, multiplyResult			;display multiplication result
 call	WriteDec
 call	Crlf
 
 ;display the result of dividing the numbers
-mov		eax, firstInt					;display first integer
+mov	eax, firstInt					;display first integer
 call	WriteDec
-mov		edx, OFFSET divisionSign		;display division sign
+mov	edx, OFFSET divisionSign		;display division sign
 call	WriteString
-mov		eax, SecInt						;display second integer
+mov	eax, SecInt						;display second integer
 call	WriteDec
-mov		edx, OFFSET equalSign			;display equal sign
+mov	edx, OFFSET equalSign			;display equal sign
 call	WriteString
-mov		eax, divisionResult				;display result of dividing numbers
+mov	eax, divisionResult				;display result of dividing numbers
 call	WriteDec
-mov		edx, OFFSET prompt3				;show the word remainder
+mov	edx, OFFSET prompt3				;show the word remainder
 call	WriteString
-mov		eax, remainder					;display remainder in number form
+mov	eax, remainder					;display remainder in number form
 call	WriteDec
 call	Crlf							;new Line
 
 ;display space
-mov		edx, OFFSET sentSpace
+mov	edx, OFFSET sentSpace
 call	WriteString
 call	Crlf
 
 ;display last prompt asking if user is impressed and printing goodbye
-mov		edx, OFFSET prompt4				;calling prompt 4
+mov	edx, OFFSET prompt4				;calling prompt 4
 call	WriteString
 call	Crlf							;newLine
 
-	;exit	; exit to operating system
-	Invoke ExitProcess, 0
+;exit	; exit to operating system
+Invoke ExitProcess, 0
 main ENDP
 
 ; (insert additional procedures here)
